1ddd3881847f262ef3002872b3c74b68ca438f7c
Ewout Eggens <ewouteggens@hotmail.com>
2020-02-26 19:00:24 +0100

    Added recursive checking for logs
    - Archive logs recursively in all folders in the specified folder
    - Delete empty folders unless they're symlinked

--- logcleaner.py
+++ logcleaner.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 from datetime import date, datetime
 
-from os import listdir, remove
-from os.path import basename, isfile, join
+from os import listdir, remove, removedirs
+from os.path import basename, dirname, isdir, isfile, islink, join
 
 import tarfile
+
 
 def main():
     log_folder = '/var/www/python/tmp'
@@ -15,15 +16,20 @@ def main():
     make_archive_of_current_logs(get_file_names_in_folder(log_folder, 'log'), archive_folder, current_date)
     remove_old_archives(archive_folder, current_date, days_to_keep_archives)
 
+
 def make_archive_of_current_logs(file_names, archive_folder, current_date):
     with tarfile.open(join(archive_folder, current_date) + ".tar.gz", "w:gz") as tar:
         for file_name in file_names:
-            tar.add(file_name, arcname=basename(file_name))
+            tar.add(file_name, arcname=file_name)
     remove_old_logs(file_names)
+
 
 def remove_old_logs(file_names):
     for file_name in file_names:
         remove(file_name)
+        if len(listdir(dirname(file_name))) == 0 and not islink(dirname(file_name)):
+            removedirs(dirname(file_name))
+
 
 def remove_old_archives(archive_folder, current_date, days_to_keep_archives):
     archives = get_file_names_in_folder(archive_folder, 'tar.gz')
@@ -34,11 +40,25 @@ def remove_old_archives(archive_folder, current_date, days_to_keep_archives):
         if (current_date - file_date).days > days_to_keep_archives:
             remove(archive)
 
+
 def get_file_names_in_folder(log_folder, extension):
-    return [join(log_folder, file) for file in listdir(log_folder) if isfile(join(log_folder, file)) and file.endswith('.' + extension)]
+    files = get_file_names_recursive(log_folder)
+    return [join(log_folder, file) for file in files if isfile(join(log_folder, file)) and file.endswith('.' + extension)]
+
+
+def get_file_names_recursive(log_folder):
+    files = []
+    for x in listdir(log_folder):
+        if isdir(join(log_folder, x)):
+            files.extend(get_file_names_recursive(join(log_folder, x)))
+        elif isfile(join(log_folder, x)):
+            files.append(join(log_folder, x))
+    return files
+
 
 def get_current_date():
     return date.today().strftime("%d-%m-%Y")    
+
 
 if __name__ == "__main__":
     main()
